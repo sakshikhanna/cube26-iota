079d1ad60de224d0f5bc9141fc10b223a388cb7e
Sakshi <khanna.sakshi09@gmail.com>
2015-10-14 23:08:12 +0530

    changed image slider

--- src/js/libs/main.js
+++ src/js/libs/main.js
@@ -1,31 +1,8 @@
-// // 
 
-// $(document).ready(function(){
-// 	// $(".fs-image-container").hover(function(){
-// 	// 	console.log('here');
-//  //       $(".fs-image-container").addClass("hide");
-//  //       $(".fs-image-container-blur").removeClass("hide");
-//  //   });
-//    $(".fs-image-container").mouseout(function(){
-//    		console.log('there');
-//        // $("p").css("background-color", "lightgray");
-//         $(".fs-image-container").removeClass("hide");
-//        $(".fs-image-container-blur").addClass("hide");
-//    });
-// $('.fs-image-container img').hover(
-//     function(){
-//     	console.log(this);
-//       $(this).attr('src','/images/feature1-blur.jpg');
-//     },
-//     function(){
-//     	console.log(this);
-//       $(this).attr('src','/images/feature1.jpg')
-//     }
-// )
-// })
 
 $( document ).ready( function(){
    //for scrolling
+    $('.banner').unslider();
    var dom = {
     mainButton : '.js-mobile-nav-button',
     navContainer : '.js-mobile-nav',
@@ -39,4 +16,9 @@ $( document ).ready( function(){
     $( dom.closeButton ).on( 'click', function() {
         $( dom.navContainer ).toggleClass( 'isOpen' );
     });
+
+
+    $(function() {
+    $('.banner').unslider();
+});
 });
