30d79bb8412d6223d6613daea3f2215c34f5304b
Sakshi <khanna.sakshi09@gmail.com>
2015-10-15 01:17:48 +0530

    minor change in slider

--- src/js/libs/main.js
+++ src/js/libs/main.js
@@ -2,7 +2,15 @@
 
 $( document ).ready( function(){
    //for scrolling
-    $('.banner').unslider();
+    // $('.banner').unslider();
+$('.banner').unslider({
+  speed: 500,               //  The speed to animate each slide (in milliseconds)
+  delay: 3000,              //  The delay between slide animations (in milliseconds)
+  complete: function() {},  //  A function that gets called after every slide animation
+  keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+  dots: true,               //  Display dot navigation
+  fluid: false              //  Support responsive design. May break non-responsive designs
+});
    var dom = {
     mainButton : '.js-mobile-nav-button',
     navContainer : '.js-mobile-nav',
@@ -16,10 +24,6 @@ $( document ).ready( function(){
     $( dom.closeButton ).on( 'click', function() {
         $( dom.navContainer ).toggleClass( 'isOpen' );
     });
-
-
-
-
 
 
     $(function() {
