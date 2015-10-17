e65acbe40bf207d658b819776e67562b3236764b
Sakshi <khanna.sakshi09@gmail.com>
2015-10-17 15:28:51 +0530

    added image slider and background color change

--- src/js/libs/main.js
+++ src/js/libs/main.js
@@ -3,14 +3,24 @@
 $( document ).ready( function(){
    //for scrolling
     // $('.banner').unslider();
-$('.banner').unslider({
-  speed: 500,               //  The speed to animate each slide (in milliseconds)
-  delay: 3000,              //  The delay between slide animations (in milliseconds)
-  complete: function() {},  //  A function that gets called after every slide animation
-  keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-  dots: true,               //  Display dot navigation
-  fluid: false              //  Support responsive design. May break non-responsive designs
-});
+    // 
+    $('.bxslider').bxSlider({
+      speed:500,
+      infiniteLoop:true,
+      hideControlOnEnd:true,
+      auto:true,
+      video: true,
+      useCSS: false 
+
+    });
+  $('.banner').unslider({
+    speed: 500,               //  The speed to animate each slide (in milliseconds)
+    delay: 3000,              //  The delay between slide animations (in milliseconds)
+    complete: function() {},  //  A function that gets called after every slide animation
+    keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+    dots: true,               //  Display dot navigation
+    fluid: false              //  Support responsive design. May break non-responsive designs
+  });
    var dom = {
     mainButton : '.js-mobile-nav-button',
     navContainer : '.js-mobile-nav',
@@ -27,6 +37,42 @@ $('.banner').unslider({
 
 
     $(function() {
-      $('.banner').unslider();
-  });
+        $('.banner').unslider();
+    });
+  
+     // $(".color5").mouseover(function() { 
+     //        $('.bg').attr("src","../../Images/Philips_Hue_1_pink.jpg");
+     //        // $(this).attr("src", src);
+     //    })
+     //     $(".color5").mouseout(function() {
+     //         $('.bg').attr("src","../../Images/Philips_Hue_1.jpg");
+     //    });
+    $('.color5').click(function(e){
+        e.preventDefault();
+         $('.bg').fadeOut(300, function() {
+            $('.bg').attr("src","../../Images/Philips_Hue_1_pink.jpg");
+            $('.bg').fadeIn(300);
+        });
+    })
+    $('.color4').click(function(e){
+        e.preventDefault();
+         $('.bg').fadeOut(300, function() {
+            $('.bg').attr("src","../../Images/Philips_Hue_1.jpg");
+            $('.bg').fadeIn(300);
+        });
+    })
+    $('.color1').click(function(e){
+        e.preventDefault();
+         $('.bg').fadeOut(300, function() {
+            $('.bg').attr("src","../../Images/Philips_Hue_1_green.jpg");
+            $('.bg').fadeIn(300);
+        });
+    })
+    $('.color2').click(function(e){
+        e.preventDefault();
+         $('.bg').fadeOut(300, function() {
+            $('.bg').attr("src","../../Images/Philips_Hue_1_red.jpg");
+            $('.bg').fadeIn(300);
+        });
+    })
 });
