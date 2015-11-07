2fda081a28488bd723f8c11d364a07c74bb3cba7
Sakshi <khanna.sakshi09@gmail.com>
2015-11-07 12:47:45 +0530

    changed backgroung image and added links

--- src/js/libs/main.js
+++ src/js/libs/main.js
@@ -22,18 +22,25 @@ console.log("here");
       controls:true
     });
 
-
+  var slider1 = $('.bxslider1').bxSlider({
+      auto:true,
+      hideControlOnEnd:true,
+      pager:false,
+      controls:true
+    });
 
     $('.btn-play').click(function(){
       console.log('showing1111111');
       $('.videoContainer').addClass('show');
+      $('.bx-controls-direction').addClass('hide');
       $( 'body' ).addClass('bodyClass');
       slider.stopAuto();
-      $('.videoContainer iframe').attr('src', 'https://www.youtube.com/embed/P6QIpEabg-U');
+      $('.videoContainer iframe').attr('src', 'https://www.youtube.com/embed/8bYouS7r8h0');
     })
     $( '.video-close' ).on( 'click', function() {
         $( '.videoContainer' ).addClass( 'hide' );
          $( '.videoContainer' ).removeClass( 'show' );
+         $('.bx-controls-direction').addClass('show');
          $('.videoContainer iframe').attr('src', '');
          $( 'body' ).removeClass('bodyClass');
     });    
